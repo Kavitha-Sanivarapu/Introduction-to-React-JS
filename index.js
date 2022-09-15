63b1c513256390e4e8c40b1926ffce5471a53f52
Kavitha-Sanivarapu <kavitha.sanivarapu94@gmail.com>
2022-09-15 07:30:55 +0000

    To do App Assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const rootElement = document.getElementById("root");
+
+const element = (
+  <div>
+    <h1 class="greeting">Hello World!</h1>
+    <p>Nice to meet you.</p>
+  </div>
+);
+
+ReactDOM.render(element, rootElement);
